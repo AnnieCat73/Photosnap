0a5490f53bc83c0bf53ba9a23fb03f544479af07
annie_i <anniecats673@gmail.com>
2021-11-18 14:26:00 +0000

    Dropdown menu in mobile

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
+//To open and close menu in mobile
+
 const menuBtn = document.querySelector(".open");
 const closeBtn = document.querySelector(".close");
-//let menuOpen = false;
+const dropdown = document.querySelector(".dropdown-menu");
+
 
 menuBtn.addEventListener("click", () => {
 
   menuBtn.style.display = "none";
   closeBtn.style.display = "block";
+  dropdown.style.display = "block";
 
 });
 
 closeBtn.addEventListener("click", () => {
   menuBtn.style.display = "block";
   closeBtn.style.display = "none";
+  dropdown.style.display = "none";
+
 });
